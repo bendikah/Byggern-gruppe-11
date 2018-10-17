515be0e060071be1216baf31ec5e73ec86c55d38
robertkt <robertkt@stud.ntnu.no>
2018-10-17 18:04:15 +0200

    Fix warnings for node 2

--- node_2/main.c
+++ node_2/main.c
@@ -4,18 +4,9 @@
 
 #include "spi.h"
 #include "can.h"
-#define F_CPU 16000000
+#include "global_defines.h"
 
 #include "util/delay.h"
-//#define FOSC 1968500// Clock Speed
-#define BAUD 9600
-#define MYUBRR F_CPU/16/BAUD-1
-
-#define set_bit(reg, bit) (reg |= (1 << bit))
-#define clear_bit( reg, bit ) (reg &= ~(1 << bit))
-#define test_bit( reg, bit ) (reg & (1 << bit))
-#define loop_until_bit_is_set( reg, bit ) while( !test_bit( reg, bit ) )
-#define loop_until_bit_is_clear( reg, bit ) while( test_bit( reg, bit ) )
 
 
 int main(void){
