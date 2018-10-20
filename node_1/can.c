56ebcbcc4486f58429cb115e4625b3761e9972a4
robertkt <robertkt@stud.ntnu.no>
2018-10-20 18:23:12 +0200

    change some canstuff on node1

--- node_1/can.c
+++ node_1/can.c
@@ -10,7 +10,7 @@
 void can_init(uint8_t mode){
     spi_initialize();
     mcp_reset();
-    /*
+
     switch (mode) {
       case 0:
       mcp_bit_modify(MCP_CANCTRL,0xE0,MODE_LOOPBACK);
@@ -18,8 +18,8 @@ void can_init(uint8_t mode){
       case 1:
       mcp_bit_modify(MCP_CANCTRL,0xE0,MODE_NORMAL);
     }
-    */
-    mcp_bit_modify(MCP_CANCTRL,0xE0,MODE_LOOPBACK); //set mode in canctrl register for now loopback only 3 first bits count see 10.4 mcp
+
+    //mcp_bit_modify(MCP_CANCTRL,0xE0,MODE_LOOPBACK); //set mode in canctrl register for now loopback only 3 first bits count see 10.4 mcp
     mcp_bit_modify(MCP_RXB0CTRL, 0x60, MCP_FILTER_OFF); //recieve all messages regardless of value see mcp 4.2.2
     mcp_bit_modify(MCP_RXB0CTRL, 0x04, MCP_ROLLOVER_OFF); //turn of overflow if 2 messages recieved
     mcp_bit_modify(MCP_CANINTE, 0xFF, MCP_RX_INT); //interrupt if message recieved on the __INT pin
@@ -44,16 +44,15 @@ void can_transmit(can_message* msg){
     mcp_request_to_send(1);
 }
 
-can_message can_recieve(void){
-    can_message msg;
+void can_recieve(can_message* msg){
 
     //if (mcp_read(MCP_CANINTF) & (MCP_RX0IF)){ // if something on the channel
         USART_printf("get some message");
         //Get message id
-      msg.id = (mcp_read(MCP_RXB0SIDH) << 3) | (mcp_read(MCP_RXB0SIDL) >> 5);
-		  msg.length = (mcp_read(MCP_RXB0DLC)) & (0x0F);
-		  for (uint8_t i = 0; i < msg.length; i++){
-			       msg.data[i] = mcp_read(MCP_RXB0D0 + i);
+      msg->id = (mcp_read(MCP_RXB0SIDH) << 3) | (mcp_read(MCP_RXB0SIDL) >> 5);
+		  msg->length = (mcp_read(MCP_RXB0DLC)) & (0x0F);
+		  for (uint8_t i = 0; i < msg->length; i++){
+			       msg->data[i] = mcp_read(MCP_RXB0D0 + i);
 		}
     mcp_bit_modify(MCP_CANINTF, MCP_RX0IF, 0x00); //turn off interrupt of recieved
 
@@ -61,7 +60,6 @@ can_message can_recieve(void){
 
     //}
 
-    return msg;
 
 }
 
--- node_1/main.c
+++ node_1/main.c
@@ -21,14 +21,14 @@ int main(void){
   set_bit(MCUCR,SRE);
   //set_bit(MCUCR, ISC01);
   //fdevopen(USART_Transmit, USART_Receive);
-  joystick_init();
+  //joystick_init();
 
 	oled_initialize();
 //_delay_ms(5000);
   SRAM_test();
   //testThisShit();
   //test_adc();
-	//menu_init();
+//menu_init();
   USART_printf("STARTING \n");
   oled_sram_print("hello world");
 
@@ -56,14 +56,15 @@ int main(void){
   USART_printf("this is the shit from mcp %x",readval);
 
   */
+
+  can_init(1);
   can_message msg;
   msg.id = 1;
   msg.length = 3;
-  msg.data[0] = 1;
+  msg.data[0] = 7;
   msg.data[1] = 2;
-  msg.data[2] = 3;
+  msg.data[2] = 0xC;
   USART_printf("CAN TEST STARTING");
-  can_init(1);
 
   can_message new_msg;
   int i = 0;
@@ -72,11 +73,13 @@ int main(void){
     i++;
     can_transmit(&msg);
     _delay_ms(2000);
-    new_msg = can_recieve();
+    /*
+    can_recieve(&new_msg);
     USART_printf("(can_msg_t){id:%x, len:%d, data:{",new_msg.id, new_msg.length);
     for(int i = 0; i < new_msg.length; i++){
   		  USART_printf(", %x", new_msg.data[i]);
   	}
+    */
     USART_printf("test\n");
   }
 
