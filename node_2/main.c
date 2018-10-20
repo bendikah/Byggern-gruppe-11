c13e520c947b82b1d801555371ae828ad400dd60
robertkt <robertkt@stud.ntnu.no>
2018-10-20 18:26:07 +0200

    some shit

--- node_2/main.c
+++ node_2/main.c
@@ -7,35 +7,59 @@
 #include "global_defines.h"
 
 #include "util/delay.h"
-
+#include "pwm.h"
+#include "servo_driver.h"
 
 int main(void){
-  USART_Init ( MYUBRR );
-  set_bit(MCUCR,SRE);
-  spi_initialize();
-  mcp_reset();
+    USART_Init ( MYUBRR );
+    set_bit(MCUCR,SRE);
+    spi_initialize();
+    //mcp_reset();
+    /*while(1){
+        /*set_bit(PORTB,SS);
+        _delay_ms(1000);
+        clear_bit(PORTB,SS);
+        _delay_ms(1000);
+        //mcp_reset();
 
-  can_init(0);
+        _delay_ms(2000);
+        mcp_write(0x03, 0xD0);
+        uint8_t ret = mcp_read(0x03);
+        USART_printf("%x\n",ret);
+    }
+
+  */
+  //can_init(0);
 
 
-  can_message msg;
+  /*can_message msg;
   msg.id = 1;
   msg.length = 3;
   msg.data[0] = 1;
   msg.data[1] = 2;
-  msg.data[2] = 3;
+  msg.data[2] = 3;*/
   USART_printf("CAN TEST STARTING \n");
-  can_init(1);
-
-  can_message new_msg;
+  //can_init(1);
+  pwm_init();
+  servo_set_angle(-100);
+  int i = 0;
   while(1){
-    can_transmit(&msg);
+
     _delay_ms(2000);
-    new_msg = can_recieve();
+    /*can_recieve(&new_msg);
     USART_printf("(can_msg_t){id:%x, len:%d, data:{",new_msg.id, new_msg.length);
     for(int i = 0; i < new_msg.length; i++){
   		  USART_printf(", %x", new_msg.data[i]);
-  	}
-    USART_printf("\n");
-  }
+  	}*/
+    USART_printf("ting skjer\n");
+
+
+
+
+
+
+
+    }
+
+
 }
