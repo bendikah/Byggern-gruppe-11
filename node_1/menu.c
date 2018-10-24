f2e79698cc2f3ebeaeb6049c52dd23d2eb5f8f8f
Robert Tamang <robertkt@stud.ntnu.no>
2018-10-24 22:51:56 +0200

    Create credit page
    Names flow over screen. 
    "Want more starwars-like"

--- node_1/menu.c
+++ node_1/menu.c
@@ -1,19 +1,24 @@
 
 #include "menu.h"
 #include "oled.h"
+#include "global_defines.h"
+#include "util/delay.h"
 
 
-enum {init = 0};
+enum {init = 0, credits};
 
 
 static int menu_branch;
 static int menu_page;
 
-#define NUM_OF_INIT_STRINGS		3
-const char* page_init[] = {"Eivind\n", "Robert\n", "Arne\n"};
+#define NUM_OF_INIT_STRINGS		4
+const char* page_init[] = {"Play", "High score", "Settings", "Credits"};
+#define NUM_OF_CREDIT_STRINGS	8
+const char* page_credits[] = {"Bernt Johan", "Vegard", "Kolbjørn", "Waseem", "Jo Arve", "Bendik", "Eivind", "Robert"};
 
 //----------Local functions--------///
 static void draw_page_init();
+static void draw_page_credits();
 
 void menu_init(){
 	menu_branch = 0;
@@ -27,17 +32,11 @@ void menu_draw(void){
         case init:
             draw_page_init();
             break;
+		case credits:
+			draw_page_credits();
+			break;
         default:
             draw_page_init();
-	}
-}
-
-void draw_page_init(){
-	for (int i = 0; i < NUM_OF_INIT_STRINGS; i++){
-		if (i == menu_branch){
-			oled_printf("->");
-		}
-		oled_printf("%s", page_init[i]);
 	}
 }
 
@@ -59,4 +58,36 @@ void menu_decrement_branch(){
 
 int get_menu_branch(){
 	return menu_branch;
+}
+
+void draw_page_init(){
+	for (int i = 0; i < NUM_OF_INIT_STRINGS; i++){
+		if (i == menu_branch){
+			oled_printf("->");
+		}
+		oled_printf("%s\n", page_init[i]);
+	}
+}
+
+void draw_page_credits(){
+	for (int i = 7; i >= 0; i--){
+		for (int j = 0; j < 8 - i; j++){
+			oled_goto_line(i + j);
+			oled_sram_print(page_credits[j]);
+		}
+		oled_sram_update();
+		_delay_ms(500);
+	}
+	for (int i = 1; i < NUM_OF_CREDIT_STRINGS; i++){
+		for (int j = 0; j < 8 ; j++){
+			oled_goto_line(j);
+			if (i + j >= NUM_OF_CREDIT_STRINGS){
+				break;
+			}
+			oled_sram_print(page_credits[i + j]);
+		}
+		oled_sram_update();
+		_delay_ms(500);
+	}
+
 }
