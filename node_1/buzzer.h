e947d5f780f9a5901edeb2edd0b3315b41b9e606
Robert Tamang <robertkt@stud.ntnu.no>
2018-10-26 21:52:39 +0200

    Create buzzer files

---
+++ node_1/buzzer.h
@@ -0,0 +1,27 @@
+
+#ifndef BUZZER_H
+#define BUZZER_H
+
+#define C = 261;
+#define D = 294;
+#define E = 329;
+#define F = 349;
+#define G = 391;
+#define GS = 415;
+#define A = 440;
+#define AS = 455;
+#define B = 466;
+#define CH = 523;
+#define CSH = 554;
+#define DH = 587;
+#define DSH = 622;
+#define EH = 659;
+#define FH = 698;
+#define FSH = 740;
+#define GH = 784;
+#define GSH = 830;
+#define AH = 880;
+
+void buzzer_play(int note, int duration);
+
+#endif
