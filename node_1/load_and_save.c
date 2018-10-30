714e29971a91cd12bae02e193417f45fda5cefba
Robert Tamang <robertkt@stud.ntnu.no>
2018-10-30 19:03:17 +0100

    Help functions for load and save users and highscore

--- node_1/load_and_save.c
+++ node_1/load_and_save.c
@@ -16,9 +16,24 @@ char* load_user(unsigned int number){
     return [EEPROM_read(USER(number)),EEPROM_read(USER(number)+1), EEPROM_read(USER(number)+2),'\0'];
 }
 
-char* load_high_score(unsigned int number);
+char* load_high_score_name(unsigned int number){
+    return [EEPROM_read(HIGH_SCORE(number)),EEPROM_read(HIGH_SCORE(number)+1), EEPROM_read(HIGH_SCORE(number)+2),'\0'];
 
-char* save_user(unsigned int number, char* user_name);
+}
+uint8_t load_high_score_score(unsigned int number){
+    return EEPROM_read(HIGH_SCORE(number)+3);
+}
 
-char* save_high_score(unsigned int number, char* user_name, uint8_t score);
+char* save_user(unsigned int number, char* user_name){
+    EEPROM_write(USER(number), user_name[0]);
+    EEPROM_write(USER(number+1), user_name[1]);
+    EEPROM_write(USER(number+2), user_name[2]);
+}
+
+char* save_high_score(unsigned int number, char* user_name, uint8_t score){
+    EEPROM_write(HIGH_SCORE(number), user_name[0]);
+    EEPROM_write(HIGH_SCORE(number)+1, user_name[1]);
+    EEPROM_write(HIGH_SCORE(number)+2, user_name[2]);
+    EEPROM_write(HIGH_SCORE(number)+3, score);
+}
 
--- node_1/load_and_save.h
+++ node_1/load_and_save.h
@@ -3,16 +3,18 @@
 #ifndef load_and_save_H
 #define load_and_save_H
 
-/*-----------Define adresses for usage--------------*/
-/* The 40 first adresses are users and their stats. Every four bytes is divided into 3 first as three chars (user)
-    and the fourth is how many times they have played.
-    From 40 there is 5*3 bytes which contains the 5 players with highest score.
-*/
+/*
+ * A user consists of three letters. They are saved in eeprom consentent(?) (etter hverandre).
+ * After a user (three letters) it is saved a stat which shows how many times the player has played.
+ * The highscores are saved in order of highest score to lowest score.
+ * Each highscore consists of three letters, representing the user, and a score.
+ */
 
 
 char* load_user(unsigned int number);
 
-char* load_high_score(unsigned int number);
+char* load_high_score_name(unsigned int number);
+uint8_t load_high_score_score(unsigned int number);
 
 char* save_user(unsigned int number, char* user_name);
 
