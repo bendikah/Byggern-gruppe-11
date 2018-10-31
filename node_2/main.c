3189f77a6c8dccb03ed67b8931a0e3b1dfe09505
Martin Eek Gerhardsen <martiege@stud.ntnu.no>
2018-10-31 16:23:30 +0100

    Test stuff in main

--- node_2/main.c
+++ node_2/main.c
@@ -11,9 +11,35 @@
 #include "servo_driver.h"
 #include "interrupt.h"
 
+#include "ir.h"
+
+#include "motor_driver.h"
+
 int main(void){
     USART_Init ( MYUBRR );
     set_bit(MCUCR,SRE);
+    interrupt_init();
+
+    motor_init();
+    int i = 0;
+    while (1) {
+      i++;
+      _delay_ms(2000);
+      motor_set_direction(i%2);
+      motor_set_speed(60);
+    }
+    /*
+    ir_init();
+    int i = 0;
+    while(1){
+      _delay_ms(2000);
+      uint8_t val = adc_get_value();
+      USART_printf("value = %d \n",val);
+      USART_printf("%d",i);
+      i++;
+    }
+    */
+
 
     //mcp_reset();
     /*while(1){
@@ -23,7 +49,7 @@ int main(void){
         _delay_ms(1000);
         //mcp_reset();
 
-        _delay_ms(2000);
+        _delay_ms  interrupt_init();(2000);
         mcp_write(0x03, 0xD0);
         uint8_t ret = mcp_read(0x03);
         USART_printf("%x\n",ret);
@@ -31,7 +57,7 @@ int main(void){
 
   */
 
-
+  /*
   interrupt_init();
 
   can_init(1);
@@ -43,7 +69,10 @@ int main(void){
   msg.data[0] = 7;
   msg.data[1] = 2;
   msg.data[2] = 0;
-*/
+ messageBuf[0] = (TWI_targetSlaveAddress<<TWI_ADR_BITS) | (FALSE<<TWI_READ_BIT); // The first byte must always consit of General Call code or the TWI slave address.
+          messageBuf[1] = TWI_CMD_MASTER_WRITE;             // The first byte is used for commands.
+          messageBuf[2] = myCounter;                        // The second byte is used for the data.
+TWI_Start_Transceiver_With_Data( messageBuf, 3 );
   //pwm_init();
   can_message new_msg;
   //servo_set_angle(-100);
@@ -66,6 +95,6 @@ int main(void){
 
 
   //Test joystick signal
-
+  */
 
 }
