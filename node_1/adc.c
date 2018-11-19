b6c6abfc18e275324380951d87ba5a7cce9388cc
Robert Tamang <robertkt@stud.ntnu.no>
2018-11-19 21:44:11 +0100

    Delete a delay

--- node_1/adc.c
+++ node_1/adc.c
@@ -8,7 +8,6 @@ volatile char *start_adc = (char *) 0x1400;
 
 uint8_t adc_read(int channel){
 	start_adc[0] = channel;
- 	_delay_ms(50); //do we need this?????????!!!!!!!!!!!!!!!111
 	return start_adc[0];
 }
 
